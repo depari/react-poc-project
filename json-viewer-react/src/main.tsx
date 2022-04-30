1edd669d9bc6a942b402096dd49952a110299a99
seo <cryingfor@gmail.com>
2022-04-30 09:14:44 +0900

    apply 4 way navigation
    using spatial-navigation-js

--- json-viewer-react/src/main.tsx
+++ json-viewer-react/src/main.tsx
@@ -3,12 +3,36 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import SpatialNavigation from 'spatial-navigation-js'
 
 declare global {
   interface Window { runStartupTasks: Function; }
 }
+
+(function () { 
+  console.log("Setup SpatialNaviation");
+  // Initialize
+  SpatialNavigation.init();
+  console.log("SpatialNavigation.init();");
+  // Define navigable elements (anchors and elements with "focusable" class).
+  SpatialNavigation.add({
+    selector: 'a, .focusable, button, input',
+  });
+  console.log("SpatialNavigation.add");
+  // Make the *currently existing* navigable elements focusable.
+  SpatialNavigation.makeFocusable();
+  console.log("SpatialNavigation.makeFocusable");
+  // Focus the first navigable element.
+  SpatialNavigation.focus();
+  console.log("SpatialNavigation.focus");
+
+})()
+
+
+
 window.runStartupTasks = () => {
   console.log("runStartupTasks  !!!!");
+  
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -16,6 +40,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   ReactDOM.createRoot(document.getElementById('root')!).render(
     // <React.StrictMode>
+
+    
       <App />
     // </React.StrictMode>
   );
